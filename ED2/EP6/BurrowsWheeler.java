afeac403634d8d26e096ae025ba9bdb6513f71c7
henrique cerquinho <henriquecerquinho@gmail.com>
2018-05-31 15:36:08 -0300

    burrows ok mas memoria explodindo

--- ED2/EP6/BurrowsWheeler.java
+++ ED2/EP6/BurrowsWheeler.java
@@ -29,10 +29,43 @@ public class BurrowsWheeler {
         int first = BinaryStdIn.readInt();
 
         String transform = BinaryStdIn.readString();
-        
+        int length = transform.length();
+        int next[] = new int[length];
+
+        // vetor de chars, é a string transform ordenada
         char[] sorted = transform.toCharArray();
         Arrays.sort(sorted);
 
+        char prev = sorted[0];
+        int seguidos = 0;
+        for (int i = 0; i < length; i++) {
+            char c = sorted[i];
+
+            if (c != prev) seguidos = 1;
+            else seguidos++;
+
+            int pos; // posição que o char c está na string transform
+            int count; // contador para quantas vezes ja achamos o
+            // char sendo procurado
+            for (pos = 0, count = seguidos; pos < length; pos++) {
+                if (transform.charAt(pos) == c) {
+                    count--;
+                    if (count == 0)
+                        break;
+                }
+            }
+            next[i] = pos;
+            prev = c;
+        }
+
+        // printa o texto original
+        int index = first;
+        for (int i = 0; i < length; i++) {
+            char c = sorted[index];
+            BinaryStdOut.write(c);
+            index = next[index];
+        }
+        BinaryStdOut.close();
     }
 
     // if args[0] is '-', apply Burrows-Wheeler transform
